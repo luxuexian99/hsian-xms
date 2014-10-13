6d4966cafd662722ecc521d87834b63a9bfd9134
Hsian <luxuexian99@gmail.com>
2014-10-13 19:29:32 +0800

    log4j.proprerties 完善，打印log测试。

--- src/main/java/org/hsian/xms/repositories/UserRepositoryImpl.java
+++ src/main/java/org/hsian/xms/repositories/UserRepositoryImpl.java
@@ -2,10 +2,14 @@ package org.hsian.xms.repositories;
 
 import org.hsian.xms.model.User;
 import org.hsian.xms.repositories.mapper.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+
+import static org.slf4j.LoggerFactory.*;
 
 /**
  * User 访问数据库的实现。
@@ -14,19 +18,22 @@ import java.util.Date;
 @Repository
 public class UserRepositoryImpl implements UserRepository {
 
+    private static final Logger logger = getLogger(UserRepositoryImpl.class);
+
     @Autowired
     private UserMapper userMapper;
 
     @Override
     public User addUser(User user) {
-
+        logger.info("addUser:" + user);
         user.setCreatedTime(new Date());
         int effective = userMapper.addUser(user);
 
         if(effective != 1) {
+            logger.error("effective error!");
 //            throw new Exception();
         }
-
+        logger.info("addUser");
         return user;
     }
 
